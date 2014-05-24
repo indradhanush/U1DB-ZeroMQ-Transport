307689b52174d24108cc047f2749e22822ff6f51
indradhanush <indradhanush.gupta@gmail.com>
2014-05-24 19:16:57 +0530

    Added SUB socket to client. Restructured client code.

--- src/client.py
+++ src/client.py
@@ -5,42 +5,82 @@ import zmq
 import settings
 
 
+class ClientSocket(object):
+    """
+    Base class for sockets at the client.
+    """
+    def __init__(self, socket, endpoint):
+        self.socket = socket
+        self.endpoint = endpoint
+
+    def run(self):
+        self.socket.connect(self.endpoint)
+
+
+class Dealer(ClientSocket):
+    """
+    zmq.DEALER socket.
+    Used for sending requests/updates to zmq.ROUTER socket on the server side.
+    """
+    def __init__(self, endpoint, context):
+        self.socket = context.socket(zmq.DEALER)
+        super(Dealer, self).__init__(self.socket, endpoint)
+
+    def run(self):
+        super(Dealer, self).run()
+
+
+class Subscriber(ClientSocket):
+    """
+    zmq.SUB socket.
+    Used for receiving updates from server through a zmq.PUB socket on the
+    server side.
+    """
+    def __init__(self, endpoint, context):
+        self.socket = context.socket(zmq.SUB)
+        super(Subscriber, self).__init__(self.socket, endpoint)
+
+    def run(self):
+        super(Subscriber, self).run()
+
+
 class Client():
     """
     Client Instance. Uses zmq.DEALER socket.
     """
-    def __init__(self, endpoint):
-        self.endpoint = endpoint
+    def __init__(self, endpoint_router, endpoint_pub):
         self.context = zmq.Context()
-        self.frontend = None
-        self.backend = None
+        self.dealer = Dealer(endpoint_router, self.context)
+        self.sub = Subscriber(endpoint_pub, self.context)
 
     def run(self):
-        self.frontend = self.context.socket(zmq.DEALER)
-        self.frontend.connect(self.endpoint)
+        # Start Dealer and Subscriber instances.
+        self.dealer.run()
+        self.sub.run()
 
         poll = zmq.Poller()
-        poll.register(self.frontend, zmq.POLLIN )
+        poll.register(self.dealer.socket, zmq.POLLIN)
 
         for i in xrange(10):
             # Simulating different requests for now. Will be removed.
             import random
-            self.frontend.send("Some request %d." % (random.randint(1, 10)))
-        
+            self.dealer.socket.send("Test Request %d." % (i+1))
+        print "Requests sent."
         while True:
             sockets = dict(poll.poll(1000))
             # If there are incoming messages.
-            if sockets.get(self.frontend) == zmq.POLLIN:
-                print self.frontend.recv()
+            if sockets.get(self.dealer.socket) == zmq.POLLIN:
+                print self.dealer.socket.recv()
             
     def tearDown(self):
-        self.frontend.close()
+        self.dealer.socket.close()
+        self.sub.socket.close()
         self.context.term()
-        self.frontend = None
+        self.dealer = None
         self.context = None
 
 
 if __name__ == "__main__":
-    client = Client(settings.ENDPOINT)
+    client = Client(settings.ENDPOINT_ROUTER, settings.ENDPOINT_PUB)
     client.run()
 
--- src/settings.py
+++ src/settings.py
@@ -2,5 +2,7 @@
 Settings for Alternate Transport to U1DB Sync Protocol.
 """
 
-PORT = "6666"
-ENDPOINT = "tcp://127.0.0.1:%s" % (PORT)
+PORT_ROUTER = "6666"
+PORT_PUB = "6667"
+ENDPOINT_ROUTER = "tcp://127.0.0.1:%s" % (PORT_ROUTER)
+ENDPOINT_PUB = "tcp://127.0.0.1:%s" % (PORT_PUB)
