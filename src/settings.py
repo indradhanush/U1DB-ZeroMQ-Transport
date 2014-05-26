dc578097df619525aec0651a0a771556b7cbf4a5
indradhanush <indradhanush.gupta@gmail.com>
2014-05-26 23:06:21 +0530

    Added soledad sockets for transferring data from application logic to server. Incomplete work.

--- src/settings.py
+++ src/settings.py
@@ -1,8 +1,14 @@
 """
-Settings for Alternate Transport to U1DB Sync Protocol.
+Project Settings
 """
 
+# Server Settings
 PORT_ROUTER = "6666"
 PORT_PUB = "6667"
 ENDPOINT_ROUTER = "tcp://127.0.0.1:%s" % (PORT_ROUTER)
 ENDPOINT_PUB = "tcp://127.0.0.1:%s" % (PORT_PUB)
+
+# Application Settings
+PORT_UPDATES = "7777"
+ENDPOINT_UPDATES = "tcp://127.0.0.1:%s" % (PORT_UPDATES)
+
---
+++ src/soledad.py
@@ -0,0 +1,60 @@
+# ZeroMQ Imports
+import zmq
+
+# Local Imports
+import settings
+from keyvaluemsg import KeyValueMsg
+
+
+class SoledadSocket(object):
+    """
+    Base class for sockets at SOLEDAD.
+    """
+    def __init__(self, socket, endpoint):
+        self.socket = socket
+        self.endpoint = endpoint
+
+    def run(self):
+        pass
+
+# TODO: zmq.DEALER socket for now. Maybe a PUSH/PULL combo later on. 
+# See: IRC logs: http://bit.ly/1tczZJC
+# TODO: Or a ROUTER/DEALER combo. 
+# See: IRC logs: http://bit.ly/1ijJxNJ
+class Dealer(SoledadSocket):
+    """
+    zmq.DEALER socket.
+    Used for handling data from Application Logic to send to zmq.PUB socket
+    at the server.
+    """
+    def __init__(self, endpoint, context):
+        self.socket = context.socket(zmq.DEALER)
+        super(Dealer, self).__init__(self.socket, endpoint)
+        
+    def run(self):
+        self.socket.connect(self.endpoint)
+        super(Dealer, self).run()
+
+
+def simulator(socket, n):
+    """Simulates updates to Publisher. For testing purposes."""
+    from random import randint
+    key = b"TEST"
+
+    for i in xrange(n):
+        request_string = b"Random Update from SOLEDAD side DEALER %d." % (randint(1, 100))
+        socket.send_multipart([key, request_string])
+        
+    print "Updates Sent."
+
+
+def main():
+    context = zmq.Context()
+    updates = Dealer(settings.ENDPOINT_PUB, context)
+    updates.run()
+    simulator(updates.socket, 5)
+
+
+if __name__ == "__main__":
+    main()
+
