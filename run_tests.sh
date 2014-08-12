730354233b577038d5306c761618ba20541678c6
indradhanush <indradhanush.gupta@gmail.com>
2014-08-12 16:31:37 +0530

    Squashed minor commits: New test code + dependency cleanup.
    Improved script to run tests.
    
    Some tests for zmq_transport.client.zmq_client.
    
    Cleaned up requirements.txt

--- run_tests.sh
+++ run_tests.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
-printf "Initiating environment..."
-if [ -d "./database/" ]; then
-    rm -rf ./database/*
-else
-    mkdir ./database
-fi
 
-# Initiate databases.
-python create_test_db.py
-printf "Done\n"
+function initiate_env() {
+    printf "Initiating environment..."
+    if [ -d "./database/" ]; then
+        rm -rf ./database/*
+    else
+        mkdir ./database
+    fi
 
+    # Initiate databases.
+    python create_test_db.py
+    printf "Done\n"
+}
+
+
+printf "##################### STAGE 1 #####################\n"
+initiate_env
 # Run tests.
 printf "Running tests..."
-
 coverage run --source zmq_transport/ -m unittest -v zmq_transport.tests.app.test_zmq_app
+
+
+# printf "##################### STAGE 2 #####################\n"
+# initiate_env
+# # Run tests.
+# printf "Running tests..."
+# coverage run --source zmq_transport/ -m unittest -v zmq_transport.tests.client.test_zmq_client
---
+++ zmq_transport/tests/client/test_zmq_client.py
@@ -0,0 +1,145 @@
+"""
+Test module for zmq_transport.client.zmq_client
+"""
+# System imports
+import unittest
+
+# Dependencies' imports
+from mock import (
+    patch
+)
+import zmq
+from zmq.eventloop.ioloop import IOLoop
+from zmq.eventloop.zmqstream import ZMQStream
+
+# Local imports
+from zmq_transport.client.zmq_client import (
+    ClientSocket,
+    Speaker,
+    ZMQClientBase,
+    Subscriber
+)
+from zmq_transport.server.zmq_server import Server
+from zmq_transport.config.settings import (
+    ENDPOINT_CLIENT_HANDLER,
+    ENDPOINT_PUBLISHER,
+    ENDPOINT_APPLICATION_HANDLER,
+)
+
+class BaseClientTest(unittest.TestCase):
+    """
+    Base test class for zmq_transport.client.zmq_client tests.
+    """
+    pass
+
+
+# class ClientSocketTest(BaseClientTest):
+#     """
+#     Test suite for zmq_client.ClientSocket
+#     """
+#     def setUp(self):
+#         self.endpoint = ENDPOINT_CLIENT_HANDLER
+#         self.context = zmq.Context()
+#         self.req = self.context.socket(zmq.REQ)
+
+
+#     def test___init__(self):
+#         """
+#         Tests ClientSocket.__init__
+#         """
+#         with patch.object(ClientSocket, "__init__") as mock_init:
+#             mock_init.return_value = None
+#             client_socket = ClientSocket(self.req, self.endpoint)
+#             mock_init.assert_called_once_with(self.req, self.endpoint)
+
+#     def test_run(self):
+#         """
+#         Tests ClientSocket.run
+#         """
+#         # with patch("zmq_transport.client.zmq_client.ClientSocket") as mock_client:
+#         #     client_socket = mock_client(self.req, self.endpoint)
+#         #     client_socket.run()
+#         #     client_socket.run.assert_called_once()
+
+#         # with patch.object(ClientSocket, "run") as mock_run:
+#         #     mock_run.return_value = None
+#         #     client_socket = ClientSocket(self.req, self.endpoint)
+#         #     client_socket.run()
+#         #     mock_run.assert_called_once()
+
+#     def tearDown(self):
+#         self.req.close()
+#         self.context.term()
+
+
+# class SpeakerTest(BaseClientTest):
+#     """
+#     Test suite for zmq_transport.client.zmq_client.Speaker
+#     """
+#     def setUp(self):
+#         self.endpoint = ENDPOINT_CLIENT_HANDLER
+#         self.context = zmq.Context()
+
+#     def test___init__(self):
+#         with patch.object(Speaker, "_socket.setsockopt") as mock_setsockopt:
+#             speaker = Speaker(self.endpoint, self.context)
+#             self.assertEqual(mock_setsockopt.call_args_list,
+
+#     def tearDown(self):
+#         pass
+
+
+class SubscriberTest(BaseClientTest):
+    """
+    Space holder for tests for zmq_transport.client.zmq_client. Future
+    of this part is unknown.
+    """
+    pass
+
+
+class ZMQClientBaseTest(BaseClientTest):
+    """
+    Test suite for zmq_transport.client.zmq_client.ZMQClientBase
+    """
+    def setUp(self):
+        self.client = ZMQClientBase(ENDPOINT_CLIENT_HANDLER,
+                             ENDPOINT_PUBLISHER)
+        # TODO: Replace self.server with mock.
+        self.server = Server(ENDPOINT_APPLICATION_HANDLER,
+                             ENDPOINT_CLIENT_HANDLER,
+                             ENDPOINT_PUBLISHER)
+
+    def test___init__(self):
+        """
+        Tests ZMQClientBase.__init__
+        """
+        self.assertIsInstance(self.client, ZMQClientBase,
+                              "Instance is not of type ZMQClientBase.")
+        self.assertIsInstance(self.client.speaker, Speaker,
+                              "Instance is not of type Speaker.")
+        self.assertIsInstance(self.client.updates, Subscriber,
+                              "Instance is not of type Subscriber.")
+
+    def test__prepare_reactor(self):
+        """
+        Tests ZMQClientBase._prepare_reactor
+        """
+        client = self.client
+        client._prepare_reactor()
+        self.assertIsInstance(client._loop, IOLoop)
+        self.assertIsInstance(client.speaker._socket, ZMQStream)
+        self.assertIsInstance(client.updates._socket, ZMQStream)
+
+    def test_client_speaker_send(self):
+        self.server.frontend.run()
+        speaker = self.client.speaker
+        speaker._socket.connect(speaker._endpoint)
+        speaker._socket.setsockopt(zmq.SNDTIMEO, 1000)
+        ret = speaker._socket.send("Hello World")
+        self.assertIsNone(ret, msg="zmq.Socket.send failed on client.speaker")
+
+    def test_client_speaker_recv(self):
+        pass
+
+    def test_client_updates_recv(self):
+        pass
