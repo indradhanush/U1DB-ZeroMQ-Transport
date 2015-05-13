94273f2ab0cb7ad780982c0c74ee1fd69abe9927
Indradhanush Gupta <indradhanush.gupta@gmail.com>
2015-05-13 07:39:53 +0530

    Add missing imports. Remove unused ones.

--- client_runner.py
+++ client_runner.py
@@ -1,34 +1,34 @@
+# System imports
+import sys
 import os.path
 
+# Third party imports
 import u1db
-import leap
 
-from zmq_transport.u1db.zmq_target import ZMQSyncTarget
-from zmq_transport.config.settings import (
-    ENDPOINT_CLIENT_HANDLER,
-    ENDPOINT_PUBLISHER,
-    DATABASE_ROOT
-)
+# Local imports
 from zmq_transport.client.sync import ZMQSynchronizer
+from zmq_transport.common.errors import UserIDNotSet
+from zmq_transport.config import settings
+from zmq_transport.u1db.zmq_target import ZMQSyncTarget
+
 
 if __name__ == "__main__":
-    import logging
-
-
-    sync_client = ZMQSyncTarget([ENDPOINT_CLIENT_HANDLER,
-                                 ENDPOINT_PUBLISHER])
+    sync_client = ZMQSyncTarget([
+        settings.ENDPOINT_CLIENT_HANDLER, settings.ENDPOINT_PUBLISHER
+    ])
 
     sync_client.set_user_id("USER-1")
 
     try:
-        import pdb
         sync_client.start()
     except UserIDNotSet as e:
         print "Aborting:", e
         sys.exit()
 
     source = u1db.open(
-        os.path.join(DATABASE_ROOT, "source-USER-1.u1db"), create=False)
+        os.path.join(settings.DATABASE_ROOT, "source-USER-1.u1db"),
+        create=False
+    )
 
     synchronizer = ZMQSynchronizer(source, sync_client)
     import time
